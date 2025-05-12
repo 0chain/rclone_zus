7f145231802fac70ad9c916d0be387b822e82108
Hitenjain14 <hitenjain146@gmail.com>
2025-05-13 00:51:46 +0530

    add zus backend

---
+++ rclone.go
@@ -0,0 +1,16 @@
+// Sync files and directories to and from local and remote object stores
+//
+// Nick Craig-Wood <nick@craig-wood.com>
+package main
+
+import (
+	_ "github.com/hitenjain14/rclone_zus/backend/zus" // your out of tree backend
+	_ "github.com/rclone/rclone/backend/all"          // import all backends
+	"github.com/rclone/rclone/cmd"
+	_ "github.com/rclone/rclone/cmd/all"    // import all commands
+	_ "github.com/rclone/rclone/lib/plugin" // import plugins
+)
+
+func main() {
+	cmd.Main()
+}
